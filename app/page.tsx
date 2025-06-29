2687ff9bfd4a6c4ac77bfa210ad3e54edfe16c12
Nawaid Sheikh <md.nawaidsheikh@gmail.com>
2025-06-29 22:22:26 +0530

    changed page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+// app/page.tsx
 import dynamic from "next/dynamic";
 
-const ClientPage = dynamic(() => import("@/app/ClientPage"), { ssr: false });
+const ClientPage = dynamic(() => import("./ClientPage"), { ssr: false });
 
 export default function HomePage() {
   return <ClientPage />;
