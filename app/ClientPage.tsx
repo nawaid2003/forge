2a9f9dff56d77cb487283dd83a20f3a536bd927d
Nawaid Sheikh <md.nawaidsheikh@gmail.com>
2025-06-29 22:26:06 +0530

    client temp

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -1,15 +1,6 @@
 // app/ClientPage.tsx
 "use client";
 
-import { DataProvider } from "@/contexts/data-context";
-import MainContent from "./MainContent";
-import Tutorial from "@/components/tutorial";
-
 export default function ClientPage() {
-  return (
-    <DataProvider>
-      <MainContent />
-      <Tutorial />
-    </DataProvider>
-  );
+  return <div>Client Page</div>;
 }
